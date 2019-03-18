667c7dc737674f180730588315408553b50ad2ae
Megan Allerton <45626633+ma6417@users.noreply.github.com>
2019-03-18 15:26:41 +0000

    changes to return options
    added 'frown' option for 'what' sign, removed 'neutralNeutral' check as same result as final 'else' statement

--- main_code.py
+++ main_code.py
@@ -4,7 +4,7 @@
 # ENSURE THAT ALL OTHER SCRIPTS ARE IN SAME DIRECTORY AS CODE, OR THAT PATH IS
 # CHANGED
 
-import facial_landmarks_video
+import facial_landmarks_videos_copy
 import Pre_Processing
 
 shapePredictor = 'shape_predictor_68_face_landmarks.dat'
@@ -12,17 +12,20 @@ shapePredictor = 'shape_predictor_68_face_landmarks.dat'
 # name of video file may be different - currently set to test.mp4 in pre-processing program
 videoFile = 'test.mp4'
 
-facialCue = facial_landmarks_videos.faceMoves(shapePredictor, videoFile)
+facialCue = facial_landmarks_videos_copy.faceMoves(shapePredictor, videoFile)
 
+print(facialCue)
 Pre_Processing.processVideo(videoFile, 'processedvideo', 30, 36, 0)
 #facial_cue : headshake, eyebrow_raised, frowning
 sign = 'hungry'
 
 if facialCue == 'shakeNeutral':
     print('not', sign)
-elif facialCue == 'neutralNeutral':
-    print(sign)
 elif facialCue == 'neutralRaised':
     print(sign+'?')
 elif facialCue == 'shakeRaised':
     print('not', sign+'?')
+elif sign == 'what' and facialCue == 'neutralFrown':
+    print(sign+'?')
+else:
+    print(sign)
